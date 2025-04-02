281dacb5f2d7be5e2a00b108613bb0794caefe0d
Tymek <tymek_uramek@wp.pl>
2025-04-02 13:33:58 +0200

    testing

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -11,10 +11,10 @@ contract ManualToken {
     function totalSupply() public pure returns(uint256){
         return 100 ether;
     }
-    function decimals() public pure retunrs(uint8) {
+    function decimals() public pure returns(uint8) {
         return 18;  
     }
-    function balanceOf(address _owner) public view retunrs(uint256){
+    function balanceOf(address _owner) public view returns(uint256){
         return s_balances[_owner];
     }
     function transfer(address _to, uint256 _amount) public {
---
+++ test/MyToken.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployMyToken} from "../script/DeployMyToken.s.sol";
+import {MyToken} from "src/MyToken.sol";
+
+contract MyTokenTest is Test {
+    MyToken public myToken;
+    DeployMyToken public deployer;
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("Alice");
+    function setUp() public{
+        deployer = new DeployMyToken();
+        myToken = deployer.run();
+
+        vm.prank(msg.sender);
+        myToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public {
+        assertEq(STARTING_BALANCE, myToken.balanceOf(bob));
+    }
+}
