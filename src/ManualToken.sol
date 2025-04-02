07ca8bed246e36a81e7166eb9629c62acff5a24a
Tymek <tymek_uramek@wp.pl>
2025-04-02 12:17:29 +0200

    lol

---
+++ src/ManualToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ManualToken {
+
+    mapping(address => uint256) private s_balances;
+    // function name() public view retunrs(string memory){
+    //     return "Manual token";
+    // }
+    string public name = "Manual Token";
+    function totalSupply() public pure returns(uint256){
+        return 100 ether;
+    }
+    function decimals() public pure retunrs(uint8) {
+        return 18;  
+    }
+    function balanceOf(address _owner) public view retunrs(uint256){
+        return s_balances[_owner];
+    }
+    function transfer(address _to, uint256 _amount) public {
+        uint256 previousBalance = balanceOf(msg.sender) + balanceOf(_to);
+        s_balances[msg.sender] -= _amount;
+        s_balances[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalance);
+    }
+}
